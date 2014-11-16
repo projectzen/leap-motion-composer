b59bfe5b2de6849d4dfd474c616a3da50c38929e
afuhrtrumpet@gmail.com <afuhrtrumpet@gmail.com>
2014-11-15 23:09:13 -0800

    Beat detection working

--- js/conduct.js
+++ js/conduct.js
@@ -1,5 +1,17 @@
-function conduct(frame) {
-    console.log("look ma I'm getting run");
+var previousPosition = [];
+
+window.measure = [0,new Date().getTime()];
+window.currentbeat = 0;
+
+function bpm_from_measure() {
+	window.measure[0] = window.measure[1];
+	window.measure[1] = new Date().getTime();
+	var result = 240000/(window.measure[1]-window.measure[0]);
+	return result;
+};
+
+function changeTempo(bpm) {
+	console.log("The new tempo is " + bpm);
 }
 
 conductController = new Leap.Controller({
@@ -10,5 +22,23 @@ conductController = new Leap.Controller({
 conductController.connect();
 
 conductController.on('frame', function(frame) {
-    console.log("3");
+	for (var i in frame.fingers) {
+		if (frame.fingers[i].type == 1) {
+			var position = frame.fingers[i].tipPosition;
+			if (previousPosition) {
+				if (window.currentbeat % 2 == 0 && previousPosition[1] < position[1] - 10)  {
+					window.currentbeat = window.currentbeat + 1;
+					console.log("Beat " + window.currentbeat);
+				}
+				else if (window.currentbeat % 2 == 1 && previousPosition[1] > position[1] + 10)  {
+					window.currentbeat = (window.currentbeat + 1) % 8;
+					console.log("Beat " + window.currentbeat);
+					if (window.currentbeat == 0) {
+						changeTempo(bpm_from_measure());
+					}
+				}
+			}
+			previousPosition = position;
+		}
+	}
 });
