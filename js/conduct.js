7fdd381a0b12417703146b4aa4e4aec816e259a8
William Osler <william@oslers.us>
2014-11-15 23:40:58 -0500

    checkpoint

--- js/conduct.js
+++ js/conduct.js
@@ -1,3 +1,14 @@
-Leap.loop(function() {
-    console.log("THE FUTURE IS CALLIN ME");
+function conduct(frame) {
+    console.log("look ma I'm getting run");
+}
+
+conductController = new Leap.Controller({
+  frameEventName: 'animationFrame',
+  useAllPlugins: true
+});
+
+conductController.connect();
+
+conductController.on('frame', function(frame) {
+    console.log("3");
 });
